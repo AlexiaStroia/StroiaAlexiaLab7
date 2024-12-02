f02801180b04e9ab64b71d47fce3c6974af4c6c3
alexiastroia9 <alexiastroia9@gmail.com>
2024-12-02 18:43:52 +0200

    .

--- StroiaAlexiaLab7/ListPage.xaml.cs
+++ StroiaAlexiaLab7/ListPage.xaml.cs
@@ -7,6 +7,7 @@ public partial class ListPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
     async void OnSaveButtonClicked(object sender, EventArgs e)
     {
         var slist = (ShopList)BindingContext;
@@ -20,4 +21,35 @@ public partial class ListPage : ContentPage
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
     }
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
+    async void OnDeleteItemClicked(object sender, EventArgs e)
+    {
+        var selectedProduct = listView.SelectedItem as Product;
+        if (selectedProduct != null)
+        {
+            await App.Database.DeleteProductAsync(selectedProduct);
+            listView.ItemsSource = await App.Database.GetListProductsAsync(((ShopList)BindingContext).ID);
+        }
+        else
+        {
+            await DisplayAlert("Error", "Please select an item to delete.", "OK");
+        }
+
+    }
+
 }
